6e6bd5bbb25a19c86aefdcd982634129b1afa447
UlyanaZhidko <zhidko.ulyana@gmail.com>
2022-09-08 07:49:36 +0500

    add: all pages, edit style and js

--- main.js
+++ main.js
@@ -1,37 +1,8 @@
-let arrow = document.querySelectorAll(".arrow");
-for (var i = 0; i < arrow.length; i++) {
-    arrow[i].addEventListener("click", (e) => {
-        let arrowParent = e.target.parentElement.parentElement; //selecting main parent of arrow
-        arrowParent.classList.toggle("showMenu");
-
-    });
-}
-
-
-// active menu item
-let dropdown = document.getElementsByClassName("categories-item");
-for (i = 0; i < dropdown.length; i++) {
-
-    dropdown[i].addEventListener("click", function() {
-        this.classList.toggle("dropdown-active");
-        for (var i = 0; i < dropdown.length; i++) {
-            dropdown[i].classList.remove('dropdown-active');
-        }
-        this.classList.add('dropdown-active');
-    });
-    dropdown[i].addEventListener("click", (e) => {
-        let arrowParent = e.target.parentElement.parentElement; //selecting main parent of arrow
-        arrowParent.classList.toggle("showMenu");
-
-    });
-}
-
-
 // open menu
 let sidebar = document.querySelector(".sidebar");
-let sidebarBtn = document.querySelector(".open-menu");
+let sidebarBtn = document.querySelector(".close-menu");
 
 sidebarBtn.addEventListener("click", () => {
     sidebar.classList.toggle("close");
-    sidebarBtn.classList.toggle("close-menu");
+    sidebarBtn.classList.toggle("open-menu");
 });
