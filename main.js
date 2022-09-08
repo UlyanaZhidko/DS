c185605784fa5013a3a87c7f438f2eba90f48ef2
UlyanaZhidko <zhidko.ulyana@gmail.com>
2022-09-08 20:36:55 +0500

    edit: table sort; title name, add: favicon

--- main.js
+++ main.js
@@ -21,6 +21,31 @@ function openTab(evt, tabName) {
     }
     document.getElementById(tabName).style.display = "block";
     evt.currentTarget.className += " active-tablinks";
+
+    // if (tabName == 'Reciever') {
+    //     document.getElementById("Reciever_sort_defaultOpen").click();
+    // } else {
+    //     document.getElementById("sort_defaultOpen").click();
+    // }
+
+    document.getElementById(`${tabName}_defaultOpen`).click();
 }
 
 document.getElementById("defaultOpen").click();
+
+// Open table_sort tab
+function openSort(evt, sort_tabName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("sort_tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    tablinks = document.getElementsByClassName("sort_tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" sort_active", "");
+    }
+
+    document.getElementById(sort_tabName).style.display = "block";
+    evt.currentTarget.className += " sort_active";
+}
